aeeec8b0451844f8197f61c278a4bfc1ae66007c
ssfm1382 <ssfm13882@gmail.com>
2025-04-28 15:01:09 +0330

    Servo motors added and connected to edgenodes

--- EdgeNode/EdgeNode.ino
+++ EdgeNode/EdgeNode.ino
@@ -1,14 +1,17 @@
-#include <Wire.h>
+#include <Servo.h>
 
 #define SOIL_SENSOR_PIN A0
 #define DC_WATER_MOTOR_PIN 2
+#define SERVO_PIN 8
 
 
 int soilMoisture = 0;
+Servo potRotator;
 
 void setup() {
   pinMode(DC_WATER_MOTOR_PIN, OUTPUT);
   digitalWrite(DC_WATER_MOTOR_PIN, LOW);
+  potRotator.attach(SERVO_PIN);
   Serial.begin(9600);
 }
 
@@ -21,6 +24,12 @@ void loop() {
   }
   else {
     digitalWrite(DC_WATER_MOTOR_PIN, LOW);
+  }
+  if (soilMoisture > 850){
+    potRotator.write(30);
+  }
+  else{
+    potRotator.write(90);
   }
   delay(500);
 }
