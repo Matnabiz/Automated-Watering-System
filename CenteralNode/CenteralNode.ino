5cd7bc75f7a1524c605a6aabf688bc6053a8c703
ssfm1382 <ssfm13882@gmail.com>
2025-04-28 15:51:58 +0330

    Initail connection between centeral and edge nodes established

--- CenteralNode/CenteralNode.ino
+++ CenteralNode/CenteralNode.ino
@@ -1,9 +1,58 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
 
+#define EDGE_NODE_ADDRESS 8
+
+#define SOIL_MOISTURE_THRESHOLD_HIGH 800
+#define SOIL_MOISTURE_THRESHOLD_LOW 500
+
+unsigned long lastRequestTime = 0;
+const unsigned long requestInterval = 2000;
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long currentTime = millis();
+  
+  if (currentTime - lastRequestTime > requestInterval) {
+    lastRequestTime = currentTime;
 
+    int soilMoisture = requestSoilMoisture(EDGE_NODE_ADDRESS);
+    Serial.print("Edge Node Soil Moisture: ");
+    Serial.println(soilMoisture);
+
+    process(EDGE_NODE_ADDRESS, soilMoisture);
+  }
+}
+
+int requestSoilMoisture(byte nodeAddress) {
+  Wire.requestFrom(nodeAddress, 2);
+  int moisture = 0;
+  if (Wire.available() >= 2) {
+    int highByte = Wire.read();
+    int lowByte = Wire.read();
+    moisture = (highByte << 8) | lowByte;
+  }
+  return moisture;
+}
+
+
+void process(byte nodeAddress, int soilMoisture) {
+  if (soilMoisture > SOIL_MOISTURE_THRESHOLD_HIGH) {
+    sendCommand(nodeAddress, 'S');
+  } 
+  else if (soilMoisture < SOIL_MOISTURE_THRESHOLD_LOW) {
+    sendCommand(nodeAddress, 'W');
+  } 
+  else {
+    sendCommand(nodeAddress, 'N');
+  }
+}
+
+void sendCommand(byte nodeAddress, char command) {
+  Wire.beginTransmission(nodeAddress);
+  Wire.write(command);
+  Wire.endTransmission();
 }
--- EdgeNode/EdgeNode.ino
+++ EdgeNode/EdgeNode.ino
@@ -1,8 +1,10 @@
 #include <Servo.h>
+#include <Wire.h>
 
 #define SOIL_SENSOR_PIN A0
 #define DC_WATER_MOTOR_PIN 2
 #define SERVO_PIN 8
+#define SLAVE_ADDRESS 8
 
 
 int soilMoisture = 0;
@@ -12,6 +14,9 @@ void setup() {
   pinMode(DC_WATER_MOTOR_PIN, OUTPUT);
   digitalWrite(DC_WATER_MOTOR_PIN, LOW);
   potRotator.attach(SERVO_PIN);
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onRequest(sendDataToMaster);
+  Wire.onReceive(receiveCommandFromMaster);
   Serial.begin(9600);
 }
 
@@ -32,4 +37,18 @@ void loop() {
     potRotator.write(90);
   }
   delay(500);
+}
+
+void sendDataToMaster() {
+  int moistureToSend = soilMoisture;
+  Wire.write((moistureToSend >> 8) & 0xFF);
+  Wire.write(moistureToSend & 0xFF);
+}
+
+void receiveCommandFromMaster(int bytes) {
+  if (bytes > 0) {
+    char command = Wire.read();
+    Serial.print("Received Command: ");
+    Serial.println(command);
+  }
 }
