748d2d8eff11d07996253a57246b8704f5be9beb
ssfm1382 <ssfm13882@gmail.com>
2025-04-28 14:36:06 +0330

    LEDs and resistors added to model DC water motors

--- EdgeNode/EdgeNode.ino
+++ EdgeNode/EdgeNode.ino
@@ -1,11 +1,14 @@
 #include <Wire.h>
 
 #define SOIL_SENSOR_PIN A0
+#define DC_WATER_MOTOR_PIN 2
 
 
 int soilMoisture = 0;
 
 void setup() {
+  pinMode(DC_WATER_MOTOR_PIN, OUTPUT);
+  digitalWrite(DC_WATER_MOTOR_PIN, LOW);
   Serial.begin(9600);
 }
 
@@ -13,5 +16,11 @@ void loop() {
   soilMoisture = analogRead(SOIL_SENSOR_PIN);
   Serial.print("Soil Moisture Value: ");
   Serial.println(soilMoisture);
+  if (soilMoisture > 850){
+    digitalWrite(DC_WATER_MOTOR_PIN, HIGH);
+  }
+  else {
+    digitalWrite(DC_WATER_MOTOR_PIN, LOW);
+  }
   delay(500);
 }
